e481344c26c1c1539e9f6c4eb627edf80e18868a
João Daniel <jdanielnd@gmail.com>
2013-04-22 18:53:28 -0300

    add state

--- exame-crawler.rb
+++ exame-crawler.rb
@@ -10,9 +10,7 @@ module ExameCrawler
   def self.get_links
     page_num = 1
     links = []
-    # until (page = read_url(page_num)).css('table').css('a').empty?
-    10.times do
-      page = read_url(page_num)
+    until (page = read_url(page_num)).css('table').css('a').empty?
       page.css('table').css('a').each do |link|
         links << link['href']
       end
@@ -30,11 +28,13 @@ module ExameCrawler
     spans = page.css('.box_empresa').css('span')
     name = spans.select{|span| span.text=="Nome:"}.first.parent.children[3].text rescue nil
     sector = spans.select{|span| span.text=="Setor:"}.first.parent.children[3].text  rescue nil
-    city = spans.select{|span| span.text=="Cidade:"}.first.parent.children[3].text  rescue nil
+    city_state = spans.select{|span| span.text=="Cidade:"}.first.parent.children[3].text.split('-')  rescue nil
+    city = city_state[0]
+    state = city_state[1]
     phone = spans.select{|span| span.text=="Telefone:"}.first.parent.children[3].text  rescue nil
     site = spans.select{|span| span.text=="Home Page:"}.first.parent.children[3].text  rescue nil
     group = spans.select{|span| span.text=="Grupo:"}.first.parent.children[3].text  rescue nil
-    {name: name, sector: sector, city: city, phone: phone, site: site, group: group}
+    {name: name, sector: sector, city: city, state: state, phone: phone, site: site, group: group}
   end
 
   def self.save_list(file_path)
@@ -42,7 +42,7 @@ module ExameCrawler
     links = get_links
     puts "Retrieved #{links.size} links!"
     CSV.open(file_path, "wb") do |csv|
-      csv << %w(Nome Setor Cidade Telefone Site Grupo)
+      csv << %w(Nome Setor Cidade Estado Telefone Site Grupo)
       links.each_with_index do |url, index|
         info = get_info(url)
         csv << info.values.to_a
